0efd22d00791c86f9f5fb3ff294eab6479d8d047
Phillip Kataswa <65820314+Artsia@users.noreply.github.com>
2023-06-05 21:23:16 -0400

    first commit

---
+++ model.py
@@ -0,0 +1,15 @@
+
+def person():
+    a = {
+        "Name": "Phillip",
+        "age": 23,
+        "phone": "437-684-4528",
+        "edu": ["post", "high", "junior", "kid"]
+        }
+    
+    return a
+
+
+p = person()
+#print(p.get("Name"))
+
